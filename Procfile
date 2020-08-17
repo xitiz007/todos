122de306e5b8f38687429d4d3fef9f5b7d2021da
xitiz007 <kshitizrock11@gmail.com>
2020-08-17 21:42:06 +0545

    updated for heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn todo_main.wsgi --log-file -
--- todo_main/settings.py
+++ todo_main/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'wrskbhey7phx@u_v+vxz#b(v50g&*o_&x@$gyphott6_lrsfu%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['xitiz-todo-webapp.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,6 +120,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
@@ -132,5 +134,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = '***********************'
-EMAIL_HOST_PASSWORD = '*******************'
+EMAIL_HOST_USER = 'kshitizrock11@gmail.com'
+EMAIL_HOST_PASSWORD = 'nxhmaqewlsumvhrx'
