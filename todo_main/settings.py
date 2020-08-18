6719a35b6995a659cea3a82360f271670cca27fa
kshitiz baniya <47114384+xitiz007@users.noreply.github.com>
2020-08-18 09:10:03 +0545

    changed in media and static urls

--- todo_main/settings.py
+++ todo_main/settings.py
@@ -119,14 +119,17 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+MEDIA_URL = '/images/'
+
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static')
 ]
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-LOGIN_URL= '/login/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 # SMTP CONFIGURATION
 
