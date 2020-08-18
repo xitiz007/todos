6dca7d4fb6fe2e29bf5b66b30a43661db7d57f3b
kshitiz baniya <47114384+xitiz007@users.noreply.github.com>
2020-08-18 09:34:35 +0545

    Update settings.py

--- todo_main/settings.py
+++ todo_main/settings.py
@@ -119,17 +119,14 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
-MEDIA_URL = '/images/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'static'),
 ]
-
+MEDIA_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+LOGIN_URL= '/login/'
 
 # SMTP CONFIGURATION
 
